22bdb135a1d9fd09872f0ae515d2a5494c74a84c
Karthi Suresh <75553966+JuiceyBeans@users.noreply.github.com>
2024-12-04 06:20:19 +0000

    Basics

---
+++ src/main/java/com/juiceybeans/slugmo/Slugmo.java
@@ -0,0 +1,27 @@
+package com.juiceybeans.slugmo;
+
+import com.mojang.logging.LogUtils;
+import com.tterrag.registrate.Registrate;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(Slugmo.MOD_ID)
+public class Slugmo {
+
+    public static final String MOD_ID = "slugmo";
+    public static final Registrate SLUGMO_REGISTRATE = Registrate.create(MOD_ID);
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Slugmo() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+}
