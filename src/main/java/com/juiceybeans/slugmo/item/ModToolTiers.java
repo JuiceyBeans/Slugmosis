c603c4a81dc688d731cd9e54545d767cdce6f2b4
Karthi Suresh <75553966+JuiceyBeans@users.noreply.github.com>
2025-04-17 17:01:52 +0100

    Tweak Vespertine properties

--- src/main/java/com/juiceybeans/slugmo/item/ModToolTiers.java
+++ src/main/java/com/juiceybeans/slugmo/item/ModToolTiers.java
@@ -12,8 +12,9 @@ import java.util.List;
 import static net.minecraftforge.common.Tags.Blocks.NEEDS_NETHERITE_TOOL;
 
 public class ModToolTiers {
+    // Vespertine - Lower durability, higher enchantability
     public static final Tier VESPERTINE = TierSortingRegistry.registerTier(
-            new ForgeTier(4, 2671, 1.5F, 4.0F, 25,
+            new ForgeTier(4, 1561, 8.0F, 4.0F, 22,
                     NEEDS_NETHERITE_TOOL, () -> Ingredient.of(ModItems.VESPERTINE.get())),
             Slugmo.id("vespertine"), List.of(Tiers.DIAMOND), List.of());
 }
