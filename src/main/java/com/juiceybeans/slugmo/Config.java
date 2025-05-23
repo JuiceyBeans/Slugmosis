c9d7a5f90c9959d3c48dba808f009169218f01e5
Karthi Suresh <75553966+JuiceyBeans@users.noreply.github.com>
2025-05-23 13:01:38 +0100

    Capitalise Rage

--- src/main/java/com/juiceybeans/slugmo/Config.java
+++ src/main/java/com/juiceybeans/slugmo/Config.java
@@ -10,15 +10,15 @@ public class Config {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
     private static final ForgeConfigSpec.ConfigValue<Integer> RAGE_CAP =
-            BUILDER.comment("How much damage it takes to max out the rage meter (default: 55)")
+            BUILDER.comment("How much damage it takes to max out the Rage meter (default: 55)")
                     .define("rage_cap", 55);
 
     private static final ForgeConfigSpec.ConfigValue<Integer> RAGE_COOLDOWN_INTERVAL =
-            BUILDER.comment("Interval of time after which rage reduces in seconds (default: 30)")
+            BUILDER.comment("Interval of time after which Rage reduces in seconds (default: 30)")
                     .define("rage_cooldown_interval", 30);
 
     private static final ForgeConfigSpec.ConfigValue<Integer> RAGE_COOLDOWN_LOSS =
-            BUILDER.comment("How much rage reduces after the cooldown interval (default: 5)")
+            BUILDER.comment("How much Rage reduces after the cooldown interval (default: 5)")
                     .define("rage_cooldown_value", 5);
 
     static final ForgeConfigSpec SPEC = BUILDER.build();
