f38090619caed21d1d9012e2d767a9de5062bab6
Karthi Suresh <75553966+JuiceyBeans@users.noreply.github.com>
2025-08-12 12:29:17 +0400

    Start of slugmo beans

---
+++ src/main/java/com/juiceybeans/slugmo/block/SlugmoBeanBlock.java
@@ -0,0 +1,61 @@
+package com.juiceybeans.slugmo.block;
+
+import com.juiceybeans.slugmo.item.ModItems;
+import net.minecraft.core.BlockPos;
+import net.minecraft.util.Mth;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public class SlugmoBeanBlock extends CropBlock {
+    public static final IntegerProperty AGE = BlockStateProperties.AGE_3;
+    private static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[] {
+            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D),
+            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 4.0D, 16.0D),
+            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 6.0D, 16.0D),
+            Block.box(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D)
+    };
+
+    public SlugmoBeanBlock(BlockBehaviour.Properties pProperties) {
+        super(pProperties);
+        this.registerDefaultState(this.getStateDefinition().any().setValue(AGE, 0));
+    }
+
+    protected ItemLike getBaseSeedId() {
+        return ModItems.SLUGMO_BEANS.get();
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return SHAPE_BY_AGE[this.getAge(pState)];
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(AGE);
+    }
+
+    @Override
+    protected IntegerProperty getAgeProperty() {
+        return AGE;
+    }
+
+    @Override
+    public int getMaxAge() {
+        return 3;
+    }
+
+    @Override
+    protected int getBonemealAgeIncrease(Level pLevel) {
+        return Mth.nextInt(pLevel.random, 1, 3);
+    }
+}
