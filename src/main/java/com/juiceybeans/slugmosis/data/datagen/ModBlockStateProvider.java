c5baa90f5936a516d43cb2cba0d1481762425f0c
Karthi Mol <75553966+JuiceyBeans@users.noreply.github.com>
2026-03-15 20:21:28 +0000

    Add block item model for agitator

--- src/main/java/com/juiceybeans/slugmosis/data/datagen/ModBlockStateProvider.java
+++ src/main/java/com/juiceybeans/slugmosis/data/datagen/ModBlockStateProvider.java
@@ -5,26 +5,15 @@ import com.juiceybeans.slugmosis.block.GlowshroomStemBlock;
 import com.juiceybeans.slugmosis.block.LightningAgitatorBlock;
 import com.juiceybeans.slugmosis.block.ModBlocks;
 import com.juiceybeans.slugmosis.block.SlugmoBeanBlock;
-import net.minecraft.core.Direction;
 import net.minecraft.data.PackOutput;
-import net.minecraft.data.models.blockstates.MultiVariantGenerator;
-import net.minecraft.data.models.blockstates.PropertyDispatch;
-import net.minecraft.data.models.blockstates.Variant;
-import net.minecraft.data.models.blockstates.VariantProperties;
-import net.minecraft.data.models.model.ModelTemplates;
 import net.minecraft.data.models.model.TextureMapping;
-import net.minecraft.data.models.model.TextureSlot;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.client.model.generators.ConfiguredModel;
-import net.minecraftforge.client.model.generators.MultiPartBlockStateBuilder;
-import net.minecraftforge.client.model.generators.VariantBlockStateBuilder;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -37,7 +26,8 @@ public class ModBlockStateProvider extends BlockStateProvider {
     protected void registerStatesAndModels() {
         this.squareStageBlock(ModBlocks.SLUGMO_BEANS.get(), SlugmoBeanBlock.AGE);
         this.crossStageBlock(ModBlocks.GLOWSHROOM_STEM.get(), GlowshroomStemBlock.AGE);
-        this.createLightningAgitatorBlock(ModBlocks.LIGHTNING_AGITATOR.get(), LightningAgitatorBlock.POWERED);
+        this.createLightningAgitatorBlock(ModBlocks.LIGHTNING_AGITATOR.get(),
+                LightningAgitatorBlock.POWERED, LightningAgitatorBlock.SHORT_CIRCUITED);
     }
 
     private String getBlockName(Block block) {
@@ -69,17 +59,19 @@ public class ModBlockStateProvider extends BlockStateProvider {
                 }, ignored);
     }
 
-    public void createLightningAgitatorBlock(Block block, BooleanProperty powered) {
+    public void createLightningAgitatorBlock(Block block, BooleanProperty powered, BooleanProperty shortCircuited) {
         this.getVariantBuilder(block)
                 .forAllStatesExcept(state -> {
                     String poweredSuffix = state.getValue(powered) ? "_powered" : "";
-                    String topName = getBlockName(block) + "_top" + poweredSuffix;
+                    String topTexureName = getBlockName(block) + "_top" + poweredSuffix;
 
                     return ConfiguredModel.builder().modelFile(models()
-                                    .cubeTop(topName,
+                                    .cubeTop("lightning_agitator",
                                             TextureMapping.getBlockTexture(Blocks.FURNACE).withSuffix("_side"),
-                                            Slugmosis.id("block/" + topName)))
+                                            Slugmosis.id("block/" + topTexureName)))
                             .build();
-                });
+                }, shortCircuited);
+
+        simpleBlockItem(block, models().getExistingFile(Slugmosis.id("block/lightning_agitator")));
     }
 }
